0da2516ffe7dab4d1a1109a874e0bcb96d5048fd
gon <y2gon2@gmail.com>
2023-11-09 14:32:37 +0900

    #10 -  Profile 설정
    ##  환경별 Property 값 사용
     * test , developement, local, server 등 동일 application 을 상황에 따라 실행 설정이 다를 수 있다.
    * 환경별 설정 조건을 미리 application-{active profile}.yaml 로 여러 profile 설정 조건을 만들어 놓을 수 있다.
    * 만약 특별한 조건이 없다면 application.yaml 을 실행할때 적용하게 된다.
    * application-beta.yaml /  application-local.ymal
       - 다양한 profile 설정 및 실행 상황을 확인하기 위해 application-beta.yaml /  application-local.ymal 을 생성하고 각각 profile-name: 을 각각  beta / local 로 설정하였다.
    *   DemoService
        - application.yaml 내 설정 value 를 직접 code 내로 가져와서 어떤 profile 이 적용되었는지를 확인하는 내용을 추가
        -` @Value("${설정 key}") private String value;`
    
    ## Intellij 실행시 Profile 적용
    * Intellij 우측 상단 applicationtion  실행 - `...` - `edit` - `Active profiles` 란에 입력
    
    ## JAR 실행시 JVM 옵션으로 Profile 적용
    * `java -jar -Dspring.profiles.active=local module-api-0.0.1-SNAPSHOT.jar`

--- module-api/src/main/java/dev/be/moduleapi/service/DemoService.java
+++ module-api/src/main/java/dev/be/moduleapi/service/DemoService.java
@@ -1,12 +1,12 @@
 package dev.be.moduleapi.service;
 
-
 import dev.be.modulecommon.domain.Member;
 import dev.be.modulecommon.enums.CodeEnum;
 import dev.be.modulecommon.repository.MemberRepository;
 import dev.be.modulecommon.service.CommonDemoService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -14,10 +14,21 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class DemoService {
 
+    @Value("${profile-name}")
+    private String name;
+
     private final CommonDemoService commonDemoService;
     private final MemberRepository memberRepository;
 
     public String save() {
+        log.info("name: " + name);
+
+        if (name == "local") {
+            // AAA
+        } else if (name == "beta") {
+            // BBB
+        }
+
         log.info(CodeEnum.SUCCESS.getCode());
         log.info(commonDemoService.commonService());
 
