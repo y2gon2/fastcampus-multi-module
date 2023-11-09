bd62fe48e927dc2cee36243a39b7045f4212c86a
gon <y2gon2@gmail.com>
2023-11-09 10:15:06 +0900

    #8 - module-api : 통합 module 로 CRUD 작업을 위한 설정
    api 를 통해 client 로 부터 처리가 필요한 data 를 받는다. 이를 내부 service 를 통하여 module-common repository 로 보내서 영속성 처리를 할 수 있도록 전달하는 logic, 설정을 구현
    
    build.gradle
     -  해당 module 에서 repository 를 생성하지는 않지만 의존성 module repository 를 application 에서 가져올 수 있도록 @EnableJpaRepository() 가 필요하며 이를 구현하기 위해서는 JPA library 의존성이 필요
    
    application.yaml
     - repository 를 의존성 module 로 부터 가져와서 DB 영속성 작업을 진행한다. 따라서 Database 설정 및 hibernate 설정이 필요함.
    
    DemoService
     - memberRepository 를 사용하여 CRUD 작업을 구현
    
    ModuleApiApplication
     - 의존성 mouduld 의 Component Scan 을 위해  scanBasePackage 를 명시해준 것과 유사하게,  외부 module Entity, Repository 지정 및 활성화 해주어야 한다. 따라서  @EntitySca() 과 @EnableJpaRepository 를 사용한다.

--- module-api/src/main/java/dev/be/moduleapi/service/DemoService.java
+++ module-api/src/main/java/dev/be/moduleapi/service/DemoService.java
@@ -1,7 +1,9 @@
 package dev.be.moduleapi.service;
 
 
+import dev.be.modulecommon.domain.Member;
 import dev.be.modulecommon.enums.CodeEnum;
+import dev.be.modulecommon.repository.MemberRepository;
 import dev.be.modulecommon.service.CommonDemoService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,14 +15,25 @@ import org.springframework.stereotype.Service;
 public class DemoService {
 
     private final CommonDemoService commonDemoService;
+    private final MemberRepository memberRepository;
 
     public String save() {
         log.info(CodeEnum.SUCCESS.getCode());
         log.info(commonDemoService.commonService());
+
+        memberRepository.save(
+                Member.builder()
+                        .name(Thread.currentThread().getName())
+                        .build()
+        );
+
         return "save";
     }
 
     public String find() {
+        int size = memberRepository.findAll().size();
+
+        log.info("Member size: " + size);
         return "find";
     }
 
